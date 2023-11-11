8294dade2a6c92369c5a1c1439e2f400daa1bcda
Bayu Tantra Bramandhita <bayutantra28@gmail.com>
2023-11-11 16:01:51 +0700

    Implement edit room data

--- resources/views/room/edit.blade.php
+++ resources/views/room/edit.blade.php
@@ -29,25 +29,37 @@
 
             <div class="card shadow mb-4">
 
-                <form class="m-4" action="{{ route('room.store') }}" method="post">
+                <form class="m-4" action="{{ route('room.update') }}" method="post">
                     @csrf
+                    @method('PUT')
                     <input type="hidden" class="form-control" name="last_author_id" value="{{ Auth::user()->id }}"
                         id="last_author_id" aria-describedby="last_author_id" placeholder="">
+                    <input type="hidden" name="room_id" value="{{ $widget['room']->id }}">
                     <div class="form-row">
                         <div class="form-group col">
                             <label for="name">Nama</label>
-                            <input type="text" class="form-control" required name="name" id="name" aria-describedby="name"
+                            <input type="text" class="form-control" value="{{ $widget['room']->name }}" required name="name" id="name" aria-describedby="name"
                                 placeholder="">
                             <small id="name" class="form-text text-muted">Help text</small>
                         </div>
                         <div class="form-group col">
                             <label for="floor">Lokasi</label>
                             <select class="form-control" name="floor" id="inlineFormCustomSelect">
-                                <option value=1>Lt. Dasar</option>
-                                <option value=2>Lt. 2</option>
-                                <option value=3>Lt. 3</option>
-                                <option value=4>Lt. 4</option>
-                                <option value=5>Ruang Terbuka Sekolah</option>
+                                <option @if ($widget['room']->floor == 1)
+                                    selected
+                                @endif value=1>Lt. Dasar</option>
+                                <option @if ($widget['room']->floor == 2)
+                                    selected @endif
+                                 value=2>Lt. 2</option>
+                                <option @if ($widget['room']->floor == 3)
+                                    selected @endif
+                                 value=3>Lt. 3</option>
+                                <option @if ($widget['room']->floor == 4)
+                                    selected @endif
+                                 value=4>Lt. 4</option>
+                                <option @if ($widget['room']->floor == 5)
+                                    selected  @endif
+                                value=5>Ruang Terbuka Sekolah</option>
                             </select>
                             <small id="floor" class="form-text text-muted">Help text</small>
                         </div>
@@ -57,10 +69,18 @@
                             <div class="form-group">
                                 <label for="type">Kategori</label>
                                 <select class="form-control" name="type" id="inlineFormCustomSelect">
-                                    <option value="Kelas">Kelas</option>
-                                    <option value="Administrasi">Administrasi</option>
-                                    <option value="Umum">Umum</option>
-                                    <option value="Lainnya">Lainnya</option>
+                                    <option @if ($widget['room']->type == 'Kelas')
+                                        selected @endif
+                                        value="Kelas">Kelas</option> 
+                                    <option @if ($widget['room']->type == 'Administrasi')
+                                        selected @endif
+                                        value="Administrasi">Administrasi</option>
+                                    <option @if ($widget['room']->type == 'Umum')
+                                        selected @endif
+                                        value="Umum">Umum</option>
+                                    <option @if ($widget['room']->type == 'Lainnya')
+                                        selected @endif
+                                        value="Lainnya">Lainnya</option>
                                 </select>
                                 <small id="type" class="form-text text-muted">Help text</small>
                             </div>
@@ -68,7 +88,7 @@
                         <div class="col-md-4">
                             <div class="form-group">
                                 <label for="size">Ukuran Ruangan</label>
-                                <input type="number" required step="0.01" class="form-control" name="size" id="size"
+                                <input type="number" value="{{ $widget['room']->size }}" required step="0.01" class="form-control" name="size" id="size"
                                     aria-describedby="size" placeholder="">
                                 <small id="size" class="form-text text-muted">Help text</small>
                             </div>
@@ -77,8 +97,12 @@
                             <div class="form-group">
                                 <label for="size_unit">Satuan<sup>2</sup></label>
                                 <select class="form-control" name="size_unit" id="inlineFormCustomSelect">
-                                    <option value="m">Meter</option>
-                                    <option value="cm">Sentimeter</option>
+                                    <option @if ($widget['room']->size_unit == 'm')
+                                        selected @endif
+                                        value="m">Meter</option>
+                                    <option @if ($widget['room']->size_unit == 'cm')
+                                        selected @endif
+                                        value="cm">Sentimeter</option>
                                 </select>
                                 <small id="size_unit" class="form-text text-muted">Help text</small>
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -18,25 +18,32 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
+// profile
 Route::get('/profile', 'ProfileController@index')->name('profile');
 Route::put('/profile', 'ProfileController@update')->name('profile.update');
 
+// inventory
 Route::get('/inventory', 'InventoryController@index')->name('inventory');
 Route::get('/inventory/add', 'InventoryController@create')->name('inventory.create');
 Route::post('/inventory/add', 'InventoryController@store')->name('inventory.store');
 Route::get('/inventory/{id}/edit', 'InventoryController@edit')->name('inventory.edit');
 Route::put('/inventory/edit', 'InventoryController@update')->name('inventory.update');
 
+// room
 Route::get('/room', 'RoomController@index')->name('room');
 Route::get('/room/add', 'RoomController@create')->name('room.create');
 Route::post('/room/add', 'RoomController@store')->name('room.store');
+Route::get('/room/{id}/edit', 'RoomController@edit')->name('room.edit');
+Route::put('/room/edit', 'RoomController@update')->name('room.update');
 
+// issue
 Route::get('/issue', 'IssueController@index')->name('issue');
 Route::get('/issue/add', 'IssueController@create')->name('issue.create');
 Route::post('/issue/add', 'IssueController@store')->name('issue.store');
 Route::get('/issue/{id}/edit', 'IssueController@edit')->name('issue.edit');
-Route::put('/issue/edit/remove', 'IssueController@removeInven')->name('issue.remove');
 Route::put('/issue/edit', 'IssueController@update')->name('issue.update');
+
+// approve issue
 Route::get('/issue/{id}/detail', 'IssueController@detail')->name('issue.detail');
 Route::put('/issue/detail', 'IssueController@approve')->name('issue.approve');
 
